f3f6e6636a9f72889b1cb6f1b754e7276e6bcb7a
maryam <maryam_moein@hotmail.com>
2025-07-04 17:51:17 +0330

    Initial commit to add the project

---
+++ SearchAppAA/SearchAppAA/SearchAppAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppAAApp.swift
+//  SearchAppAA
+//
+//  Created by Maryam on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
